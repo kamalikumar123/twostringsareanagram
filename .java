3c13d36f73f091a4060f7281323eb8275bb198b0
kamali <93373518+kamalikumar123@users.noreply.github.com>
2021-12-18 15:10:14 +0530

    Create .java

---
+++ .java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+
+class Main {
+  public static void main(String[] args) {
+    String str1 = "Race";
+    String str2 = "Care";
+    
+    str1 = str1.toLowerCase();
+    str2 = str2.toLowerCase();
+
+    
+    if(str1.length() == str2.length()) {
+
+      
+      char[] charArray1 = str1.toCharArray();
+      char[] charArray2 = str2.toCharArray();
+
+   
+      Arrays.sort(charArray1);
+      Arrays.sort(charArray2);
+
+     
+      boolean result = Arrays.equals(charArray1, charArray2);
+
+      if(result) {
+        System.out.println(str1 + " and " + str2 + " are anagram.");
+      }
+      else {
+        System.out.println(str1 + " and " + str2 + " are not anagram.");
+      }
+    }
+    else {
+      System.out.println(str1 + " and " + str2 + " are not anagram.");
+    }
+  }
+}
